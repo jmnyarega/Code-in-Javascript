7d33835bcb67d2ce104f094195f16d4a87a2cd99
Josiah ñarega <jmnyarega@gmail.com>
2021-10-18 11:35:28 +0000

    Fix bug with Heap

--- Abstract-Ds/heap/deleteHeap.ts
+++ Abstract-Ds/heap/deleteHeap.ts
@@ -1,16 +1,44 @@
 const swap = require("./swap.ts");
 
+const getMax = (array, sibs) => {
+  const element1 = array[sibs[0]];
+  const element2 = array[sibs[1]];
+
+  if (sibs.length === 1) return sibs[0];
+  if (element1 > element2) return sibs[0];
+  return sibs[1];
+}
+
+const getMin = (array, sibs) => {
+  const element1 = array[sibs[0]];
+  const element2 = array[sibs[1]];
+
+  if (sibs.length === 1) return sibs[0];
+  if (element1 > element2) return sibs[1];
+  return sibs[0];
+}
+
 module.exports = (array, type) => {
   const deleted = array.shift();
   let n = array.length - 1;
   let i = n;
 
   while(i > 0) {
-    let p = Math.floor((i - 1) / 2);
-    if (type === "min") {
-      array[p] > array[i] && swap(p, i, array);
+    const p = Math.floor((i - 1) / 2);
+    const sibs = [];
+
+    if (i % 2 === 0) {
+      sibs.push(i, i - 1);
     } else {
-      array[p] < array[i] && swap(p, i, array);
+      sibs.push(i);
+    }
+
+    if (type === "min") {
+      let minElement = getMin(array, sibs);
+      array[p] > array[minElement] && swap(p, minElement, array);
+    } else {
+      let maxElement = getMax(array, sibs);
+      array[p] < array[maxElement] && swap(p, maxElement, array);
     }
     i = p;
   }
--- Abstract-Ds/heap/index.ts
+++ Abstract-Ds/heap/index.ts
@@ -6,25 +6,33 @@ const deleteH = require("./deleteHeap.ts");
 class Heap {
   #collection = [];
   #type = "min";
+  #sorted = [];
 
   min = (el) => {
-    this.collection = minHeap(this.#collection, el);
+    minHeap(this.#collection, el);
     this.#type = "min";
   }
   max = (el) => {
-    this.#collection = maxHeap(this.#collection, el);
+    maxHeap(this.#collection, el);
     this.#type = "max";
   }
   print = () => console.log({ type: this.#type, data: this.#collection })
-  heapify = (array) =>  this.#collection = heapify(array);
-  deleteHeap = () => this.#collection = deleteH(this.#collection, this.#type);
+  heapify = (array) => heapify(array);
+  deleteHeap = () => {
+    const [deleted] = deleteH(this.#collection, this.#type);
+    this.#sorted.push(deleted);
+  }
+  getSorted = () => this.#sorted;
   clear = () => this.#collection = [];
 }
 
 const heap = new Heap();
 [0, -4, 2, 1, 9, -1].forEach(x => {
-  heap.max(x)
+  heap.min(x)
 })
 
-console.log(heap.deleteHeap());
-heap.print();
+for(let i = 0; i < 6; i++) {
+  heap.deleteHeap();
+}
+
+console.log(heap.getSorted())
